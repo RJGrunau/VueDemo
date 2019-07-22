7897420f7560bf7d1b3581c946dccbd23087bef9
rjgrunau <robertgrunau@gmail.com>
2019-07-22 15:18:35 -0700

    set up store

---
+++ store/index.js
@@ -0,0 +1,25 @@
+export const state = () => ({
+  flickerPhotoStream: []
+});
+
+export const mutations = {
+  updateFlickerPhotoStream: (state, payload) => {
+    state.flickerPhotoStream = payload;
+  }
+};
+
+export const actions = {
+  async getFlickerPhotoStream({ state, commit }) {
+    if (state.flickerPhotoStream.length) return;
+
+    try {
+      let flickerPhotoStream = await fetch(
+        "https://www.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=07c31cdcb84e847d37ed7ed414b66828&user_id=68066544%40N06&format=json&nojsoncallback=1"
+      ).then(res => res.json());
+    } catch (error) {
+      console.log(error);
+    }
+    commit("updateFlickerPhotoStream", flickerPhotoStream);
+    console.log(flickerPhotoStream);
+  }
+};
