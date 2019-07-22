9e4cfedb6033c1d2d0e4ad7fd4811aeedf31ce1c
rjgrunau <robertgrunau@gmail.com>
2019-07-22 16:21:57 -0700

    started header and added needed pages

---
+++ components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <div class="logo">Logo</div>
+    <nav>
+      <ul>
+        <li>
+          <nuxt-link>Gallery</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link>Projects</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link>About</nuxt-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
