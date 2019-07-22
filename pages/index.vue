f9f080b0937a451696847fede30ec30474111c46
rjgrunau <robertgrunau@gmail.com>
2019-07-22 15:31:48 -0700

    state successfully set from flicker api

--- pages/index.vue
+++ pages/index.vue
@@ -2,40 +2,31 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
-        portfolio
-      </h1>
-      <h2 class="subtitle">
-        Kick ass nuxt portfolio
-      </h2>
+      <h1 class="title">portfolio</h1>
+      <h2 class="subtitle">Kick ass nuxt portfolio</h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Logo from "~/components/Logo.vue";
+import { mapState } from "vuex";
 
 export default {
   components: {
     Logo
+  },
+  computed: {
+    ...mapState(["flickerPhotoStream"])
+  },
+  mounted() {
+    this.$store.dispatch("getFlickerPhotoStream");
   }
-}
+};
 </script>
 
 <style>
@@ -49,8 +40,8 @@ export default {
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
