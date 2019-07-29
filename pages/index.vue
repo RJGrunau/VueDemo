a26c49941a0171fcb5a8628573180f6795405b50
rjgrunau <robertgrunau@gmail.com>
2019-07-29 15:43:21 -0700

    headers

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
   <main>
+    <div class="name">
+      <h1>Paul Holly</h1>
+      <h3>Failed Developer</h3>
+      <h3>Poor Photographer</h3>
+    </div>
     <div class="camera">
       <Camera />
     </div>
@@ -24,9 +29,33 @@ main {
   grid-template-columns: repeat(6, 1fr);
   grid-template-rows: repeat(5, 1fr);
 
+  .name {
+    grid-column: 1/3;
+    grid-row: 3/4;
+    text-align: center;
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-flow: wrap column;
+
+    h1 {
+      text-transform: uppercase;
+      font-size: 2.5rem;
+      margin-bottom: 30px;
+    }
+
+    h3 {
+      font-size: 1.8rem;
+      margin-bottom: 30px;
+    }
+  }
+
   .camera {
     grid-column: 3/7;
-    grid-row: 2/6;
+    grid-row: 2/5;
+    margin-top: 100px;
+    margin-bottom: 10px;
   }
 }
 </style>
